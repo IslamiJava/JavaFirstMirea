eb6f46f6f04ce8f582f8784d72f8722284513269
IslamiJava <konorlove495@gmail.com>
2022-11-08 22:05:32 +0300

    commit

--- src/ru/mirea/task7/tretirzadanie/TestMovable1.java
+++ src/ru/mirea/task7/tretirzadanie/TestMovable1.java
@@ -2,8 +2,9 @@ package ru.mirea.task7.tretirzadanie;
 
 public class TestMovable1 {
     public static void main(String[] args) {
-        MovableRectangle rectangle = new MovableRectangle(1,3, 10, 10, 0, 0);
+        MovableRectangle rectangle = new MovableRectangle(1,3, 9, 10, 0, 0);
         System.out.println(rectangle.convertToString(12,12));
+        System.out.println("скорость");
         rectangle.speedTest();
     }
 }
